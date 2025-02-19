95a6b59cb17a453055f365dc7f4f88742adb54f7
Nguyen Xuan Truong <truongnxhe141509@fpt.edu.vn>
2025-02-19 14:29:45 +0700

    update user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,3 +19,81 @@ exports.createNewUser = async (req, res) => {
         res.status(500).json({message: 'server error', error}); 
     }
 }
+
+exports.getOneUser = async (req, res) => {
+    try {
+        console.log(req.params.userId)
+        const user = await User.findOne({_id: req.params.userId}).select(['-password', '-token']);
+        if(!user) return res.status(400).json({message: 'user not exist'});
+        res.status(200).json({user});
+    } catch (error) {
+        res.status(500).json({message: 'server error', error});
+    }
+}
+
+
+exports.updateUserInfo = async (req, res) => {
+    try {
+        const checkUser = await User.findById(req.params.userId)
+        if(req.file){ req.body.avatar = req.file.path;}
+
+        if(checkUser.avatar.startsWith('public') && req.file){ 
+            fs.unlink(checkUser.avatar, function(err){ return }) 
+        };
+        if(req.body.address){
+            req.body.$push = {address: req.body.address};
+            
+        }
+        delete req.body.password;
+        delete req.body.address;
+        const user = await User.findOneAndUpdate({_id: req.params.userId}, req.body, {new: true}).select(['-password', '-token']);
+        res.status(200).json({user});
+    } catch (error) {
+        console.log(39, error);
+        if(error.code === 11000) return res.status(400).json({message: `${Object.keys(error.keyValue)[0]} is used`, error});
+        res.status(500).json({message: 'server error', error});
+    }
+}
+
+exports.changePassword = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId);
+
+        const checkPass = await bcrypt.compare(req.body.oldPass, user.password);
+        if(!checkPass) return res.status(400).json({message: 'wrong password'});
+
+        await User.updateOne({_id: req.params.userId}, {password: req.body.newPass});
+        res.status(200).json({message: 'update success'});
+    } catch (error) {
+        res.status(500).json({message: 'server error', error});
+    }
+}
+
+// exports.changeAvatar = async (req, res) => {
+//     try {
+//         if(!req.file) return res.status(400).json({message: 'please choose an image to upload'});
+
+//         const user = await User.findById(req.user.id);
+
+//         if(checkUser.avatar.startsWith('public') && req.file){ 
+//             fs.unlink(user.avatar, (err) => {return});
+//         }
+
+//         const newUser = await User.findOneAndUpdate({_id: req.user.id}, {avatar: req.file.path}, {new: true, runValidators: true});
+//         delete newUser._doc.password;
+//         delete newUser._doc.token;
+//         res.status(200).json({newUser});
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({message: 'server error', error});  
+//     }
+// }
+
+// exports.updateAddress = async (req, res) => {
+//     try {
+//         console.log(93, req.user.id);
+//         res.status(200).json({user: req.user.id});
+//     } catch (error) {
+//         res.status(500).json({message: 'server error', error});  
+//     }
+// }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 
 router.get('/get-all-user', getAllUser);
 router.post('/create-new-user', createNewUser);
-// router.get('/get-one-user/:userId', getOneUser);
+router.get('/get-one-user/:userId', getOneUser);
 // router.patch('/update-user-info/:userId', checkLogin, upload.single('avatar'), updateUserInfo);
 // router.patch('/change-password/:userId', checkLogin, changePassword);
 // router.patch('/update-address', checkLogin, updateAddress);
