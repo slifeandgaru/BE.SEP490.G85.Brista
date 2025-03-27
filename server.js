ddec45113cc0be76c7455f3b50d8e515df0ff3ac
Nguyen Xuan Truong <truongnxhe141509@fpt.edu.vn>
2025-03-27 08:20:06 +0700

    update server

--- server.js
+++ server.js
@@ -15,9 +15,13 @@ const router = require("./startups/indexRouter")
 // }));
 
 // app.use(passport.authenticate('session'));
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173", // Đổi thành URL frontend của bạn
+    credentials: true // ⚡ Cho phép gửi cookie
+}));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('./build'));
 app.use('/public', express.static(path.join(__dirname, './public')));
 
@@ -33,6 +37,6 @@ app.use("/", router)
 //     res.send("Home")
 // })
 
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT, () => {
     console.log('server run at port', process.env.PORT);
 });
