9c9f19cae6d90e4db8865a14fd8aa74f8734d8c8
slifeandgaru <salamandersuper8@gmail.com>
2025-04-04 17:51:27 +0700

    udate

--- models/order.js
+++ models/order.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const OrderSchema = mongoose.Schema({
     userId: {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
-<<<<<<< HEAD
     userName: String,
-=======
-    customerId: {type: mongoose.Schema.Types.ObjectId, ref: 'customers'},
->>>>>>> e90fae2ef12447826747aeba5f86030e04dd7d69
     product: [{
         productId: {type: mongoose.Schema.Types.ObjectId, ref: 'products'},
         quantity: Number,
